f3c4e4b113701f690a4866c213b2888ead22923b
masumraheel <masumhossain017252@gmail.com>
2024-12-20 12:57:23 +0600

    update design

--- src/Pages/OurPackage/PackageMenu.jsx
+++ src/Pages/OurPackage/PackageMenu.jsx
@@ -2,13 +2,13 @@ const PackageMenu = ({ tour }) => {
   const { name, img, price, type } = tour;
   return (
     <div>
-      <div className="card bg-base-100 max-w-full shadow-md">
+      <div className="card bg-base-100 max-w-full shadow-sm">
           <img
             src={img}
             alt="image"
-            className="rounded-xl h-64"
+            className="rounded-sm h-64"
           />
-        <div  className="mt-2">
+        <div  className="mt-2 p-2">
           <h2 className="card-title">{name}</h2>
           <p>Price: {price}</p>
           <p>Type: {type}</p>
