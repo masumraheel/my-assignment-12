b63e1362588500313ae9120348863e9d88dcce0b
masumraheel <masumhossain017252@gmail.com>
2024-12-27 19:15:19 +0600

    testimonial section update

---
+++ src/Components/Stories/Stories.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Keyboard, Scrollbar, Navigation, Pagination } from "swiper/modules";
+
+import { ImFacebook } from "react-icons/im";
+
+const Stories = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
+  return (
+    <section className="mt-10 ">
+      <div className="text-center">
+        <h3 className="my-heading">Our Clients Story</h3>
+        <h2 className="text-center text-2xl text-amber-600 font-bold tracking-tight sm:text-3xl">
+          Read trusted reviews from our customers
+        </h2>
+      </div>
+      <div>
+        <Swiper
+          slidesPerView={1}
+          centeredSlides={false}
+          slidesPerGroupSkip={1}
+          grabCursor={true}
+          keyboard={{
+            enabled: true,
+          }}
+          breakpoints={{
+            769: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+            },
+          }}
+          scrollbar={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="card bg-base-100 w-96 shadow-xl">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes"
+                    className="rounded-xl"
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Shoes!</h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Stories;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import Services from '../Components/Servicess/Services';
 import TourType from './TourTypes/TourType';
+import Stories from '../Components/Stories/Stories';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
           <Banner></Banner>
           <Services></Services>
           <TourType></TourType>
+          <Stories></Stories>
         </div>
     );
 };
