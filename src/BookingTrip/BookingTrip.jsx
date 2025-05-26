bed34706a494d4af12e534df3cb4c709a318db22
masumraheel <masumhossain017252@gmail.com>
2025-05-26 08:22:23 +0600

    my booking trip section update

--- src/BookingTrip/BookingTrip.jsx
+++ src/BookingTrip/BookingTrip.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import Cover from '../Components/Cover/Cover';
-import bookingImage from "../assets/Cover image/booking.jpg"
+import React, { useState } from "react";
+import Cover from "../Components/Cover/Cover";
+import bookingImage from "../assets/Cover image/booking.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const BookingTrip = () => {
-    return (
-        <div>
-            <Cover img={bookingImage}></Cover>
-            This is my tour
-        </div>
-    );
+    const [tabIndex, setTabIndex]=useState(0);
+  return (
+    <div>
+      <Cover img={bookingImage}></Cover>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Adventure</Tab>
+          <Tab>Cultural</Tab>
+          <Tab>Wildlife</Tab>
+          <Tab>Luxury</Tab>
+          <Tab>Beach</Tab>
+          <Tab>Family</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default BookingTrip;
