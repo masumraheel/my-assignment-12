0353aa0e1e24285e94df13270a9a57b210ebbcc2
masumraheel <masumhossain017252@gmail.com>
2025-08-09 18:09:13 +0600

    my log in from update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center w-1/2 lg:text-left">
+        <div className="text-center md:w-1/2 lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
@@ -12,8 +19,8 @@ const Login = () => {
             a id nisi.
           </p>
         </div>
-        <div className="card bg-base-100  w-full max-w-sm  shadow-2xl">
-          <from className="card-body ">
+        <div className="card bg-base-100 md:w-1/2 max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
             <fieldset className="fieldset">
               <label className="label">Email</label>
               <input type="email" name="email" className="input" placeholder="Email" />
@@ -22,10 +29,13 @@ const Login = () => {
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
-              {/* <button className="btn btn-neutral mt-4">Login</button> */}
-              <input className="btn btn-neutral mt-4" type="submit" value="Login" />
+              <input
+                className="btn btn-neutral mt-4"
+                type="submit"
+                value="Login"
+              />
             </fieldset>
-          </from>
+          </form>
         </div>
       </div>
     </div>
