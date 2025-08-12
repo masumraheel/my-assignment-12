1e434cbd568c13b4b5ea576e910f2bce77b96836
masumraheel <masumhossain017252@gmail.com>
2025-08-12 08:44:19 +0600

    my login section update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
 
 const Login = () => {
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
   const handleLogin = (event) => {
     event.preventDefault();
     const form = event.target;
@@ -9,11 +19,17 @@ const Login = () => {
     const password = form.password.value;
     console.log(email, password);
   };
+  const handleValidateCaptcha = () => {
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)) {
+    
+    }
+  };
   return (
     <div className="bg-white dark:bg-gray-900">
       <div className="flex justify-center h-screen">
         <div
-          className="hidden bg-cover lg:block lg:w-2/3"
+          className="hidden bg-cover lg:block lg:w-1/2"
           style={{
             backgroundImage: `url('https://i.ibb.co.com/gZFXRJ1w/freepik-the-style-is-candid-image-photography-with-natural-20854.jpg')`,
           }}
@@ -34,7 +50,7 @@ const Login = () => {
           </div>
         </div>
 
-        <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-3/6">
+        <div className="flex items-center lg:w-1/2 max-w-md px-6 mx-auto">
           <div className="flex-1">
             <div className="text-center">
               <div className="flex justify-center mx-auto">
@@ -95,13 +111,38 @@ const Login = () => {
                     className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   />
                 </div>
+                <div className="mt-6">
+                  <div className="flex justify-between mb-2">
+                    <label
+                      for="password"
+                      className="text-sm text-gray-600 dark:text-gray-200"
+                    >
+                      <LoadCanvasTemplate />
+                    </label>
+                  </div>
+
+                  <input
+                    type="captcha"
+                    name="captcha"
+                    ref={captchaRef}
+                    placeholder="verify captcha"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                  <button
+                    onClick={handleValidateCaptcha}
+                    className="btn btn-xs mt-2 btn-success"
+                  >
+                    Validate
+                  </button>
+                </div>
 
                 <div className="mt-6">
                   <input
+                    disabled={disabled}
                     className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
                     type="submit"
                     value="Login"
-                    name=""
+                    name="Login"
                     id=""
                   />
                 </div>
@@ -126,5 +167,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
